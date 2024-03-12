22084d1c5e00cd2a7714684e6f4d63da3ddb9913
Arian <arian.kulmer@web.de>
2024-03-12 01:20:11 +0100

    power: supply: qti_battery_charger: Add support for 33W reverse wireless charging
    Change-Id: Ic77236a573db6d8aeef04a423d6f1e005f474bc9

--- drivers/power/supply/qti_battery_charger_xiaomi.c
+++ drivers/power/supply/qti_battery_charger_xiaomi.c
@@ -4635,6 +4635,132 @@ static ssize_t sport_mode_show(struct class *c,
 }
 static CLASS_ATTR_RW(sport_mode);
 
+#if defined(CONFIG_REVERSE_33W)
+static ssize_t downshift_show(struct class *c,
+					struct class_attribute *attr, char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct psy_state *pst = &bcdev->psy_list[PSY_TYPE_XM];
+	int rc;
+
+	rc = read_property_id(bcdev, pst, XM_PROP_DOWNSHIFT);
+	if (rc < 0)
+		return rc;
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", pst->prop[XM_PROP_DOWNSHIFT]);
+}
+
+static ssize_t downshift_store(struct class *c,
+					struct class_attribute *attr,
+					const char *buf, size_t count)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct psy_state *pst = &bcdev->psy_list[PSY_TYPE_XM];
+	int val;
+	int rc;
+
+	if (kstrtoint(buf, 10, &val))
+		return -EINVAL;
+
+	rc = write_property_id(bcdev, pst, XM_PROP_DOWNSHIFT, val);
+	if (rc < 0)
+		return rc;
+
+	return count;
+}
+static CLASS_ATTR_RW(downshift);
+
+static ssize_t snk_protocol_show(struct class *c,
+					struct class_attribute *attr, char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct psy_state *pst = &bcdev->psy_list[PSY_TYPE_XM];
+	int rc;
+
+	rc = read_property_id(bcdev, pst, XM_PROP_SNK_PROTOCOL);
+	if (rc < 0)
+		return rc;
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", pst->prop[XM_PROP_SNK_PROTOCOL]);
+}
+static CLASS_ATTR_RO(snk_protocol);
+
+static ssize_t screen_unlock_show(struct class *c,
+					struct class_attribute *attr, char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct psy_state *pst = &bcdev->psy_list[PSY_TYPE_XM];
+	int rc;
+
+	rc = read_property_id(bcdev, pst, XM_PROP_SCREEN_UNLOCK);
+	if (rc < 0)
+		return rc;
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", pst->prop[XM_PROP_SCREEN_UNLOCK]);
+}
+
+static ssize_t screen_unlock_store(struct class *c,
+					struct class_attribute *attr,
+					const char *buf, size_t count)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct psy_state *pst = &bcdev->psy_list[PSY_TYPE_XM];
+	int val;
+	int rc;
+
+	if (kstrtoint(buf, 10, &val))
+		return -EINVAL;
+
+	rc = write_property_id(bcdev, pst, XM_PROP_SCREEN_UNLOCK, val);
+	if (rc < 0)
+		return rc;
+
+	return count;
+}
+static CLASS_ATTR_RW(screen_unlock);
+
+static ssize_t reverse_thermal_show(struct class *c,
+					struct class_attribute *attr, char *buf)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct psy_state *pst = &bcdev->psy_list[PSY_TYPE_XM];
+	int rc;
+
+	rc = read_property_id(bcdev, pst, XM_PROP_REVERSE_THERMAL);
+	if (rc < 0)
+		return rc;
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", pst->prop[XM_PROP_REVERSE_THERMAL]);
+}
+
+static ssize_t reverse_thermal_store(struct class *c,
+					struct class_attribute *attr,
+					const char *buf, size_t count)
+{
+	struct battery_chg_dev *bcdev = container_of(c, struct battery_chg_dev,
+						battery_class);
+	struct psy_state *pst = &bcdev->psy_list[PSY_TYPE_XM];
+	int val;
+	int rc;
+
+	if (kstrtoint(buf, 10, &val))
+		return -EINVAL;
+
+	rc = write_property_id(bcdev, pst, XM_PROP_REVERSE_THERMAL, val);
+	if (rc < 0)
+		return rc;
+
+	return count;
+}
+static CLASS_ATTR_RW(reverse_thermal);
+#endif
+
 #ifdef CONFIG_MI_CHARGER_M81
 static ssize_t atest_store(struct class *c,
 					struct class_attribute *attr,
@@ -4951,6 +5077,12 @@ static struct attribute *xiaomi_battery_class_attrs[] = {
 #endif
 #if defined(CONFIG_MI_ENABLE_DP)
 	&class_attr_has_dp.attr,
+#endif
+#if defined(CONFIG_REVERSE_33W)
+	&class_attr_downshift.attr,
+	&class_attr_snk_protocol.attr,
+	&class_attr_screen_unlock.attr,
+	&class_attr_reverse_thermal.attr,
 #endif
 #ifndef CONFIG_MI_CHARGER_M81
 	&class_attr_thermal_board_temp.attr,
