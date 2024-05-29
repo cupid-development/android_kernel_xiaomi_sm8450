4ab9d8a9e208abec520fdef12e670a5f4c6b0770
Arian <arian.kulmer@web.de>
2024-05-29 12:23:35 +0200

    input: touchscreen: fts_spi: Get rid of factory code
    Change-Id: I6caab7cc2bdfa247fbb33697bff0ac1fc6d88f2d

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -3882,20 +3882,11 @@ static u8 fts_need_enter_lp_mode(void)
 
 	if (fts_info->aod_status && !fts_info->nonui_status)
 		tmp_value |= FOD_SINGLETAP_EVENT;
-#ifndef CONFIG_TOUCH_FACTORY_BUILD
 	if (fts_info->fod_status == 1 || fts_info->fod_status == 2) {
 		tmp_value |= FOD_LONGPRESS_EVENT;
 		if (fts_info->fod_icon_status && !fts_info->nonui_status)
 			tmp_value |= FOD_SINGLETAP_EVENT;
 	}
-#else
-	if (fts_info->fod_status != -1 && fts_info->fod_status != 100 &&
-	    fts_info->fod_status != 0 && fts_info->fod_status != 1) {
-		tmp_value |= FOD_LONGPRESS_EVENT;
-		if (fts_info->fod_icon_status && !fts_info->nonui_status)
-			tmp_value |= FOD_SINGLETAP_EVENT;
-	}
-#endif
 	return tmp_value;
 }
 
@@ -5768,7 +5759,6 @@ static int fts_mode_handler(struct fts_ts_info *info, int force)
 		logError(0, "%s %s: Screen OFF... \n", tag, __func__);
 		gesture_type = fts_need_enter_lp_mode();
 		gesture_cmd[5] = gesture_type;
-#ifndef CONFIG_TOUCH_FACTORY_BUILD
 		if (gesture_type) {
 			if (info->gesture_enabled == 1)
 				gesture_cmd[2] = 0x20;
@@ -5788,7 +5778,6 @@ static int fts_mode_handler(struct fts_ts_info *info, int force)
 			mdelay(WAIT_AFTER_LOW_POWER);
 			res |= ret;
 		} else {
-#endif
 			if (info->gesture_enabled == 1) {
 				logError(1, "%s %s: enter doubletap mode! \n",
 					 tag, __func__);
@@ -5809,9 +5798,7 @@ static int fts_mode_handler(struct fts_ts_info *info, int force)
 				ret = setScanMode(SCAN_MODE_ACTIVE, 0x00);
 				res |= ret;
 			}
-#ifndef CONFIG_TOUCH_FACTORY_BUILD
 		}
-#endif
 		setSystemResetedDown(0);
 		break;
 
@@ -5930,7 +5917,6 @@ static int fts_mode_handler(struct fts_ts_info *info, int force)
 		}
 #endif
 #ifdef FTS_FOD_AREA_REPORT
-#ifndef CONFIG_TOUCH_FACTORY_BUILD
 		if (info->fod_pressed) {
 			logError(1, "%s %s: Sense OFF \n", tag, __func__);
 			res |= setScanMode(SCAN_MODE_ACTIVE, 0x00);
@@ -5939,12 +5925,9 @@ static int fts_mode_handler(struct fts_ts_info *info, int force)
 				 __func__);
 			res |= setScanMode(SCAN_MODE_ACTIVE, 0x20);
 		} else {
-#endif
 			logError(1, "%s %s: Sense ON\n", tag, __func__);
 			res |= setScanMode(SCAN_MODE_ACTIVE, 0x01);
-#ifndef CONFIG_TOUCH_FACTORY_BUILD
 		}
-#endif
 		info->sensor_scan = true;
 		res = fts_write_dma_safe(gesture_cmd, ARRAY_SIZE(gesture_cmd));
 		if (res < OK)
@@ -7260,9 +7243,6 @@ static int fts_set_up_interrupts_mode(int enable)
 static void fts_resume_work(struct work_struct *work)
 {
 	struct fts_ts_info *info;
-#ifdef CONFIG_TOUCH_FACTORY_BUILD
-	int retval = 0;
-#endif
 	int r;
 
 	info = container_of(work, struct fts_ts_info, resume_work);
@@ -7291,30 +7271,18 @@ static void fts_resume_work(struct work_struct *work)
 
 	info->resume_bit = 1;
 
-#ifndef CONFIG_TOUCH_FACTORY_BUILD
 	fts_disableInterrupt();
 #ifdef CONFIG_SECURE_TOUCH
 	fts_secure_stop(info, true);
 #endif
-#else
-	retval = fts_enable_reg(info, true);
-	if (retval < 0) {
-		logError(1, "%s %s: ERROR Failed to enable regulators\n", tag,
-			 __func__);
-	}
-#endif
 
-#ifndef CONFIG_TOUCH_FACTORY_BUILD
 #ifdef FTS_FOD_AREA_REPORT
 	if (!info->fod_pressed) {
 #endif
-#endif
 		fts_system_reset();
 		release_all_touches(info);
-#ifndef CONFIG_TOUCH_FACTORY_BUILD
 #ifdef FTS_FOD_AREA_REPORT
 	}
-#endif
 #endif
 	fts_mode_handler(info, 0);
 	msleep(12);
@@ -7356,9 +7324,6 @@ static void fts_resume_work(struct work_struct *work)
 static void fts_suspend_work(struct work_struct *work)
 {
 	struct fts_ts_info *info;
-#ifdef CONFIG_TOUCH_FACTORY_BUILD
-	int retval = 0;
-#endif
 	int r;
 
 	info = container_of(work, struct fts_ts_info, suspend_work);
@@ -7413,16 +7378,8 @@ static void fts_suspend_work(struct work_struct *work)
 	release_all_touches(info);
 	info->fod_pressed = false;
 	info->sensor_sleep = true;
-#ifdef CONFIG_TOUCH_FACTORY_BUILD
-	retval = fts_enable_reg(info, false);
-	if (retval < 0) {
-		logError(1, "%s %s: ERROR Failed to enable regulators\n", tag,
-			 __func__);
-	}
-#else
 	if (info->gesture_enabled || fts_need_enter_lp_mode())
 		fts_enableInterrupt();
-#endif
 #ifdef CONFIG_FTS_BOOST
 	lpm_disable_for_dev(false, EVENT_INPUT);
 #endif
@@ -9222,10 +9179,6 @@ static int fts_probe(struct spi_device *client)
 	int retval;
 	int skip_5_1 = 0;
 	u16 bus_type;
-#ifdef CONFIG_TOUCH_FACTORY_BUILD
-	int res = 0;
-	u8 gesture_cmd[6] = { 0xA2, 0x03, 0x00, 0x00, 0x00, 0x03 };
-#endif
 	logError(1, "%s %s: driver spi ver: %s\n", tag, __func__,
 		 FTS_TS_DRV_VERSION);
 #ifdef I2C_INTERFACE
@@ -9670,23 +9623,7 @@ static int fts_probe(struct spi_device *client)
 
 	dev_set_drvdata(info->fts_touch_dev, info);
 #ifdef FTS_FOD_AREA_REPORT
-#ifdef CONFIG_TOUCH_FACTORY_BUILD
-	mutex_lock(&info->fod_mutex);
-	res = fts_write_dma_safe(gesture_cmd, ARRAY_SIZE(gesture_cmd));
-	if (res < OK)
-		logError(
-			1,
-			"%s %s: enter gesture and longpress failed! ERROR %08X recovery in senseOff...\n",
-			tag, __func__, res);
-	else
-		logError(1, "%s %s send gesture and longpress cmd success\n",
-			 tag, __func__);
-	fts_enableInterrupt();
-	info->fod_status = 1;
-	mutex_unlock(&info->fod_mutex);
-#else
 	info->fod_status = -1;
-#endif
 	info->fod_icon_status = 1;
 	error = sysfs_create_file(&info->fts_touch_dev->kobj,
 				  &dev_attr_fod_test.attr);
