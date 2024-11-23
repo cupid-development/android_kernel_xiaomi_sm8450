c7057d5341e6b930f004311319375b230382e6d4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-11-23 22:32:13 +0800

    /proc/pid/map_files: fake all lineage symlinks

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2271,12 +2271,18 @@ static int map_files_get_link(struct dentry *dentry, struct path *path)
 
 	rc = -ENOENT;
 	vma = find_exact_vma(mm, vm_start, vm_end);
-	if (vma && vma->vm_file) {
-		*path = vma->vm_file->f_path;
-		path_get(path);
-		rc = 0;
-	}
-	mmap_read_unlock(mm);
+ 	if (vma) {
+        	if (vma->vm_file) {
+            		if (strstr(vma->vm_file->f_path.dentry->d_name.name, "lineage")) { 
+            		rc = kern_path("/dev/ashmem (deleted)", LOOKUP_FOLLOW, path);
+        	} else {
+			*path = vma->vm_file->f_path;
+			path_get(path);
+                	rc = 0;
+            		}
+        	}
+    	}
+	up_read(&mm->mmap_sem);
 
 out_mmput:
 	mmput(mm);
